ad066db66de0fe955a7eb0b21fe5dfe63d926c82
Bruno.Castro <bruno.castro@mag-it.com.br>
2021-03-06 10:43:01 -0300

    initial commit

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MoraisFramework
+{
+    public class Class1
+    {
+        public void Log()
+        {
+            Console.WriteLine("pudim");
+        }
+
+        public string Get()
+        {
+            return "pudim";
+        }
+        
+        public string Get2()
+        {
+            return "pudim";
+        }
+    }
+}
